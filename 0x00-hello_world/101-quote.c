8dbcf2d64cb8045d02aa41c0505c12ec2f948737
ZolisaB <brianzoliswa@gmail.com>
2023-02-16 17:26:39 -0800

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,15 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 1 (Error code)
+ */
 int main(void)
 {
-    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, message, sizeof(message) - 1);
-    return 1;
+	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, message, sizeof(message) - 1);
+	return (1);
 }
 
