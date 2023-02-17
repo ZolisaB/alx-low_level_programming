de1a0700c51525d4921af424d46131a421371eb8
ZolisaB <brianzoliswa@gmail.com>
2023-02-17 12:23:21 -0800

    Success

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,6 @@
 /*
  * File: 2-print_alphabet.c
- * Auth: Brennan D Baraban
+ * Auth: ZolisaB
  */
 
 #include <stdio.h>
--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,6 +1,6 @@
 /*
  * File: 5-print_numbers.c
- * Auth: Brennan D Baraban
+ * Auth: ZolisaB
  */
 
 #include <stdio.h>
