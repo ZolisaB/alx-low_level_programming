9379fda8c3c0cbfb0889da1b613dfdc31ea2e023
ZolisaB <brianzoliswa@gmail.com>
2023-02-17 14:34:43 -0800

    success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,17 +14,4 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; <= 9; num++)
-	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
-
-		putchar(',');
-		putchar(' ');
-	}
-
-	putchar('\n');
-
-	return (0);
-}
+	for (num 
