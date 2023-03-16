4495c63f7d2860fde68c20746b7f901229fcccd2
ZolisaB <brianzoliswa@gmail.com>
2023-03-16 13:48:13 -0700

    More mollac

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,27 +2,29 @@
 #include <stdlib.h>
 
 /**
-* main - multiplies two numbers
-* @argc: number of command-line arguments
-* @argv: array of command-line arguments
-*
-* Return: 0 on success, 1 on error
+* main - multiply 2 numbers as arguments
+* @argc: number of arguments
+* @argv: arguments
+* Return: 0 success
 */
 int main(int argc, char *argv[])
 {
-int num1, num2, product;
+unsigned long mul;
+int i, j;
+	if (argc != 3)
+	{ printf("Error\n");
+	exit(98); }
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{  printf("Error\n");
+			exit(98); }
+		}
 
-if (argc != 3)
-{
-printf("Error\n");
-return (1);
-}
-
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
-product = num1 *num2;
-
-printf("%d\n", product);
-
+	}
+	mul = atol(argv[1]) *  atol(argv[2]);
+	printf("%lu\n", mul);
 return (0);
 }
