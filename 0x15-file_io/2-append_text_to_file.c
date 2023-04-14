18abc0bc468023f56a4012e4de9be33841ccce36
ZolisaB <brianzoliswa@gmail.com>
2023-04-14 13:52:28 -0700

    Append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,37 +1,42 @@
 #include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 /**
- * append_text_to_file - appends text to the end of a file.
- * @filename; name of the file to append to.
- * @text_contnt: text content to append.
- *
- * Return: 1 on success, -1 on failure.
- */int append_text_to_file(const char *filename, char *text_content)
+* append_text_to_file - appends text to the end of a file.
+* @filename: name of the file to append to.
+* @text_content: text content to append.
+*
+* Return: 1 on success, -1 on failure.
+*/
+int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, nletters = 0, rwr = 0;
+int fd, nletters = 0, rwr = 0;
 
-	if (filename == NULL)
-		return (-1);
+if (filename == NULL)
+return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
+fd = open(filename, O_WRONLY | O_APPEND);
 
-	if (fd == -1)
-		return (-1);
+if (fd == -1)
+return (-1);
 
-	if (text_content)
-	{
-		while (text_content[nletters])
-			nletters++;
+if (text_content)
+{
+while (text_content[nletters])
+nletters++;
 
-		rwr = write(fd, text_content, nletters);
+rwr = write(fd, text_content, nletters);
 
-		if (rwr == -1)
-		{
-			close(fd);
-			return (-10);
-		}
-	}
+if (rwr == -1)
+{
+close(fd);
+return (-1);
+}
+}
 
-	close(fd);
-	return (1);
-
+close(fd);
+return (1);
+}
