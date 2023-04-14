b87ce835064330e20d9c87f1c1f219e822b8575e
ZolisaB <brianzoliswa@gmail.com>
2023-04-14 13:30:40 -0700

    trial

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,35 +1,50 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
- * read_textfile - reads a text file and prints the letters.
- * @filename: the name of the file to be read.
- * @letters: the number of letters to be printed.
- *
- * Return: the number of letters printed; 0 if function fails
- */
+* read_textfile - Reads a text file and prints the specified number of letters.
+* @filename: Name of the file to read.
+* @letters: Number of letters to read and print.
+*
+* Return: The number of letters read and printed, or 0 if an error occurred.
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t n_read, n_written;
-	char *buf;
+int fd;
+ssize_t n_read, n_written;
+char *buf;
 
-	if (!filename)
-		return (0);
+if (filename == NULL)
+return (0);
 
-	fd = open(file, O_RDONLY);
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
 
-	if (fd == -1)
-		return (0);
+buf = malloc(sizeof(char) * letters);
+if (buf == NULL)
+{
+close(fd);
+return (0);
+}
 
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
-		return (0);
+n_read = read(fd, buf, letters);
+if (n_read == -1)
+{
+free(buf);
+close(fd);
+return (0);
+}
 
-	n_read = read(fd, buf, letters);
-	n_written = write(STDOUT_FILENO, buf, n_read);
+n_written = write(STDOUT_FILENO, buf, n_read);
+if (n_written == -1)
+{
+free(buf);
+close(fd);
+return (0);
+}
 
-	close(fd);
-	free(buf);
-
-	return (n_written);
+free(buf);
+close(fd);
+return (n_written);
 }
